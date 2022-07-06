209084135bd06b1289f055b7e1db7d567ec79dcf
Vlad <venediktov.92@mail.ru>
2022-07-06 16:19:58 +0300

    fixed link

--- test_items.py
+++ test_items.py
@@ -1,12 +1,9 @@
 from selenium.webdriver.common.by import By
-import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-link = "https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/reviews/add/#addreview"
-
-
-def test_guest_should_see_basket(browser):
+def test_guest_should_see_basket_button(browser):
     browser.get(link)
-    time.sleep(30)
     basket = browser.find_elements(By.CLASS_NAME, "btn-add-to-basket")
-    assert basket is not None, "No button add to basket"
+    assert basket, "No button add to basket"
